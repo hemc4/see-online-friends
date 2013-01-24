d5919bcc5de606d214b5d445365de75a37e92842
Hemc <hemendra.chaudhary619@gmail.com>
2013-01-24 17:44:10 +0530

    changed successfully

--- index.php
+++ index.php
@@ -173,7 +173,8 @@ $app_name = idx($app_info, 'name', '');
           // window.location.reload() because if this is in a canvas there was a
           // post made to this page and a reload will trigger a message to the
           // user asking if they want to send data again.
-          window.location = window.location;
+          //window.location = window.location;
+          window.location.reload();
         });
 
         FB.Canvas.setAutoGrow();
